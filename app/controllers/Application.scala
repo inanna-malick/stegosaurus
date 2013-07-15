c1dffd4be7f263c6059f7457d05f70c8e3bccf13
Paul Kinsky <pkinsky@gmail.com>
2013-07-15 14:33:37 -0400

    refactoring

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -29,9 +29,7 @@ object Application extends Controller {
       val key = "barfoo"
 
       Async {
-
-        println(msg)
-
+        
         val baos = new ByteArrayOutputStream()
 
         val b = time("build with image") {
@@ -41,29 +39,26 @@ object Application extends Controller {
           b.Compress(new ByteArrayInputStream(msg.getBytes()), key)
         }
 
-        //upload_image(baos.toByteArray()) <-- actually upload
-        upload_image_stub map { json =>
+        upload_image(baos.toByteArray()) map { json =>
           val img_url = json
           println(img_url)
           Redirect(tweet_url(img_url))
         }
       }
 
-      /*
+      
+      /* TODO: switch to choose between uploading and tweeting or just downloading img
+    
       val bais = new ByteArrayInputStream(baos.toByteArray())
       val fileContent: Enumerator[Array[Byte]] = Enumerator.fromStream(bais)
 
       val fname = picture.filename
 
-		
-      
       SimpleResult(
         header = ResponseHeader(200),
         body = fileContent).as("application/x-download").withHeaders("Content-disposition" -> s"attachment; filename=$fname")
-      
+        
       */
-
-      //Ok("foo")
 
     }.getOrElse {
       Redirect(routes.Application.index).flashing(
@@ -92,7 +87,7 @@ object Application extends Controller {
   //TODO: better auth, oauth(?)
   def upload_image(file: Array[Byte]): Future[String] = {
     WS.url("https://api.imgur.com/3/image")
-      .withHeaders(("Authorization", "Client-ID dcb5b8c68dc45f4"))
+      .withHeaders(("Authorization", "Client-ID dcb5b8c68dc45f4")) //todo: no central client id, anon upload or get user's credentials somehow
       .post(file).map({ response => println(response.json); ((response.json \ "data") \ "link").toString().replace("\"", "") })
   }
 
@@ -103,7 +98,7 @@ object Application extends Controller {
   def time[R](name: String)(block: => R): R = {
     println(s"begin timing $name")
     val t0 = System.nanoTime()
-    val result = block // call-by-name
+    val result = block
     val t1 = System.nanoTime()
     println("Elapsed time: " + (t1 - t0) + "ns")
     result
