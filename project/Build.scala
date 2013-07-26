78737e7dfac9a4aa963de18a6ee711e118b98847
Paul Kinsky <pkinsky@gmail.com>
2013-07-26 15:48:32 -0400

    removed finagle deps
    play framework provides futures, no need for finagle

--- project/Build.scala
+++ project/Build.scala
@@ -8,15 +8,12 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    "com.twitter"        %% "finagle-core"              % "6.3.0",
-    "com.twitter"        %% "finagle-http"              % "6.3.0",
     jdbc,
     anorm
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    resolvers += "twitter" at "http://maven.twttr.com/"
   )
 
 }
