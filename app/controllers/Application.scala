a1ebc1f87cd49aeed95d65a5bcdf9932e98b8c0b
Paul Kinsky <pkinsky@gmail.com>
2013-08-16 17:38:00 -0400

    error messages, profanity
    now using scalaz's validations to propagate errors through for
    comprehension instead of options.
    
    all actions rewritten to use for comprehensions
    
    key is passed in, no longer using default value

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -25,14 +25,24 @@ import play.api.mvc._
 
 import play.api.Play.current
 
-object Application extends Controller {
-
-	val key = "barfoo"
-	
+object Application extends Controller {	
 	def index = Action { Ok(views.html.index("Stegosaurus")) }
 	
+	/**
+	 * de-steg msg from img
+	 */
+	def extract_msg(key: String, img: java.io.File): Future[String] = future{
+		val msg = new ByteArrayOutputStream 
+		extract(new FileInputStream(img), img.length.toInt, msg, key)
+		msg.toString 
+		
+	}
+
 	
-	def embed(msg: String, img: BufferedImage) = Akka.future {
+	/**
+	 * Embed msg in image
+	 */
+	def embed(msg: String, key: String, img: BufferedImage) = Akka.future {
 			val img_out = new ByteArrayOutputStream
 			val encoder_widget = new JpegEncoder(img, 80, img_out, "test")
 			encoder_widget Compress(new ByteArrayInputStream(msg getBytes), key)
@@ -40,73 +50,95 @@ object Application extends Controller {
 		}
 	
 	def encrypt_and_download = Action(parse.multipartFormData) { request =>
-		println(request.body)
-		request.body.file("picture").map { picture =>
-			val img: BufferedImage = ImageIO.read(picture.ref.file)
-			val fname = picture.filename
+		val result = for {
+			img_data <- request.body.file("picture").toSuccess(e = "fuck, no picture")
+			msg <- request.body.dataParts.get("message")
+						.map(_.head).filter(_ != "").toSuccess(e = "fuck, no msg")
+			key <- request.body.dataParts.get("key")
+						.map(_.head).filter(_ != "").toSuccess(e = "fuck, no key")
+			img <- ImageIO.read(img_data.ref.file).success
+			fname <- img_data.filename.success
+			download_future <- (for {
+				stegged_img <- embed(msg, key, img)
+			} yield {
+				val bais = new ByteArrayInputStream(stegged_img.toByteArray)
+				val data: Enumerator[Array[Byte]] = Enumerator fromStream bais
+				SimpleResult(
+					header = ResponseHeader(200),
+					body = data)
+				.as("application/x-download")
+				.withHeaders("Content-disposition" -> s"attachment; filename=$fname")
+			}).success
+		} yield download_future
 
-			val msg = request.body.dataParts("message").head //should be an option, holding off until I figure out monad transformers
-			
-			
-			val stegged_img = embed(msg, img)
-			
-			Async {
-				stegged_img map { img_out =>
-					val bais = new ByteArrayInputStream(img_out.toByteArray)
-					val fileContent: Enumerator[Array[Byte]] = Enumerator fromStream bais
+		//TODO: non-awful error msg
+		val onError = (error: String) => future{ Ok(<h1>error => {error}</h1>).as("text/html") } 
 
-					SimpleResult(
-						header = ResponseHeader(200),
-						body = fileContent).as("application/x-download").withHeaders("Content-disposition" -> s"attachment; filename=$fname")
-				}
-			}
-		}.getOrElse {
-			Redirect(routes.Application.index).flashing("error" -> "Missing file")
+		Async {
+			result.fold(
+				errorResponse,
+				response => response)
 		}
 	}
 	
 	def encrypt_and_tweet = Action(parse.multipartFormData) { request =>
-			//nested for comprehensions make me sad.
-			val result = for {
-				img_data <- request.body.file("picture")
-				img <- ImageIO.read(img_data.ref.file).some
-				fname <- img_data.filename.some
-				msg <- request.body.dataParts.get("message").map(_.head)
-				tweet_future <- (for {
-					stegged_img <- embed(msg, img)
-					img_url <- upload_image(stegged_img.toByteArray)
-				} yield Redirect(s"https://twitter.com/intent/tweet?url=$img_url")).some
-			} yield tweet_future
-			
-			Async{
-				result.getOrElse(future{Redirect(routes.Application.index).flashing("error" -> "Missing file")})
-			}
+		val result = for {
+			img_data <- request.body.file("picture").toSuccess(e = "fuck, no picture")
+			msg <- request.body.dataParts.get("message")
+						.map(_.head).filter(_ != "").toSuccess(e = "fuck, no msg")
+			key <- request.body.dataParts.get("key")
+						.map(_.head).filter(_ != "").toSuccess(e = "fuck, no key")
+			img <- ImageIO.read(img_data.ref.file).success
+			fname <- img_data.filename.success
+			tweet_future <- (for {
+				stegged_img <- embed(msg, key, img)
+				img_url <- upload_image(stegged_img.toByteArray)
+			} yield Redirect(s"https://twitter.com/intent/tweet?url=$img_url")).success
+		} yield tweet_future
+
+		//TODO: non-awful error msg
+		val onError = (error: String) => future{ Ok(<h1>error => {error}</h1>).as("text/html") } 
+
+		Async {
+			result.fold(
+				errorResponse,
+				response => response)
+		}
 	}
 	
 
 	def decrypt = Action(parse.multipartFormData) { request =>
-		request.body.file("picture").map { picture =>
-			val img_in = picture.ref.file
-			val str_out = new ByteArrayOutputStream
-			extract(new FileInputStream(img_in), img_in.length.toInt, str_out, key)
-
-			Ok(str_out.toString)
-		}.getOrElse {
-			Redirect(routes.Application.index).flashing("error" -> "Missing file")}
+		val result = for {
+			img_data <- request.body.file("picture").toSuccess(e = "fuck, no picture")
+			key <- request.body.dataParts.get("key")
+						.map(_.head).filter(_ != "").toSuccess(e = "fuck, no key")
+			img <- img_data.ref.file.success
+			msg_future <- (for { 
+					msg <- extract_msg(key, img)
+				} yield Ok(<h1>message => {msg}</h1>).as("text/html")).success
+		} yield msg_future
+		
+		Async {
+			result.fold(
+				errorResponse,
+				response => response)		
+		}
 	}
 
-	//upload image to imgur, return json
+	/**
+	 * upload image to imgur anonymously, return URL of hosted image
+	 * TODO: type coercion of json response
+	 * URL-validating return type
+	 */
 	def upload_image(file: Array[Byte]): Future[String] = {
-		WS.url("https://api.imgur.com/3/image")
-	.withHeaders(("Authorization", "Client-ID dcb5b8c68dc45f4"))
-	.post(file).map{ response =>
-	 ((response.json \ "data") \ "link").toString.drop(1).dropRight(1) //trim enclosing quotes
+			WS.url("https://api.imgur.com/3/image")
+			.withHeaders(("Authorization", "Client-ID dcb5b8c68dc45f4"))
+			.post(file).map{ response =>
+			 ((response.json \ "data") \ "link").toString.drop(1).dropRight(1) //trim enclosing quotes
 	}
 	}
-
-	//mock imgur link to avoid hammering their upload api
-	val upload_image_stub = future { "http://i.imgur.com/8DbaCGd.jpg" }
-
+	
+	val errorResponse = (error: String) => future{ Ok(<h1>error => {error}</h1>).as("text/html") }
 }
 	
 	
